7c1695e8590c2ca575889dcce1acda6673aaa032
Pegasus321 <87698717+Pegasus321@users.noreply.github.com>
2023-08-07 23:45:31 +0530

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { NAMED_ENTITIES } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { Car } from './cars';
 
 @Component({
   selector: 'my-app',
@@ -7,8 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  subaru: Car = { make: 'Subaru', model: 'Outback', miles: 58232 };
+  honda: Car = { make: 'Honda', model: 'Accord', miles: 39393 };
+  bmw: Car = { make: 'BMW', model: 'X3', miles: 4400 };
+  cars: Car[] = [this.subaru, this.honda, this.bmw];
   prefix = 'I am a';
   name = 'star student';
+  colors: string[] = ['red', 'blue', 'green', 'purple'];
 
   speak() {
     const statement = this.prefix + this.name;
