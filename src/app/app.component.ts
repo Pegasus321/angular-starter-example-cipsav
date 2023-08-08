692389c419cd837b37135305ceffd413a7364dd6
Pegasus321 <87698717+Pegasus321@users.noreply.github.com>
2023-08-08 21:56:53 +0530

    added angular transportation service.....where transportationService is a private variable that is of the type TransportationService.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { NAMED_ENTITIES } from '@angular/compiler';
 import { Component } from '@angular/core';
 import { Car } from './cars';
+import { TransportationService } from './transportation.service';
 
 @Component({
   selector: 'my-app',
@@ -8,16 +9,21 @@ import { Car } from './cars';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  subaru: Car = { make: 'Subaru', model: 'Outback', miles: 58232 };
-  honda: Car = { make: 'Honda', model: 'Accord', miles: 39393 };
-  bmw: Car = { make: 'BMW', model: 'X3', miles: 4400 };
-  cars: Car[] = [this.subaru, this.honda, this.bmw];
-  prefix = 'I am a';
-  name = 'star student';
-  colors: string[] = ['red', 'blue', 'green', 'purple'];
-
-  speak() {
-    const statement = this.prefix + this.name;
-    return statement;
+  cars: Car[];
+  constructor(private transportationService: TransportationService) {
+    this.cars = this.transportationService.getCars();
   }
+  //now instead of here we import it from transportaion.service.ts
+  // subaru: Car = { make: 'Subaru', model: 'Outback', miles: 58232 };
+  // honda: Car = { make: 'Honda', model: 'Accord', miles: 39393 };
+  // bmw: Car = { make: 'BMW', model: 'X3', miles: 4400 };
+  // cars: Car[] = [this.subaru, this.honda, this.bmw];
+  // prefix = 'I am a';
+  // name = 'star student';
+  // colors: string[] = ['red', 'blue', 'green', 'purple'];
+
+  // speak() {
+  //   const statement = this.prefix + this.name;
+  //   return statement;
+  // }
 }
